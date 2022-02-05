9ff521aec7b15a957ebcd71ae7d8ce9318e089d1
JRM <johnrhmartin@gmail.com>
2022-02-05 16:44:20 -0500

    added city and 5 day forecast cards. added an event listener to the Search button and output a console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+var citySearchEl = document.querySelector("#button-addon2"); 
+
+var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").append(document.createTextNode(day));
+
 var getWeatherData = function(input) {
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=5&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
     console.log(apiUrl);
-}
+};
 
-getWeatherData("London");
+var buttonClickHandler = function(event) {
+    console.log("button-clicked");
+};
+
+citySearchEl.addEventListener("click", buttonClickHandler);
