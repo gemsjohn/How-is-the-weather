8c8790c02655216a7f96cac7e992caaf543a369b
JRM <johnrhmartin@gmail.com>
2022-02-05 23:06:38 -0500

    append temperature, wind, humidity, and UV index data to the CITY card.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,99 @@
+// Variables that query the index.html file
 var citySearchEl = document.querySelector("#button-addon2"); 
-var inputValue = document.querySelector("#search-location");
-var displayResults = document.querySelector("#results");
+var inputValueEl = document.querySelector("#search-location");
+var displayResultsEl = document.querySelector("#results");
+var weatherDetails = document.querySelector("#weather-details");
 
-var latitude = 0;
-var longitude = 0;
-
+// Establish the current date and time
 var day = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").append(document.createTextNode(day));
 
-var convertGeoToData = function(lat, lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&exclude=hourly,dailer&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
+// When the user searches for a city, the input value 
+// serves as a parameter for the getWeatherData function
+var buttonClickHandler = function() {
+    var input = inputValueEl.value.trim();
+    getWeatherData(input);
+};
 
-}
-
+// getWeatherData takes the 'input' and adds it to the apiUrl query
+// If the user gets a response then use the data
+// as the parameter for displaySearchResults function
+// else display an alert
 var getWeatherData = function(input) {
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=5&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function(data) {
+                    displaySearchResult(data);
+                });
+            } else {
+                alert("Error: " + response.statusText)
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to OpenWeather");
+        });
+};
+
+// displaySearchReach takes the 'city' geocoding api data, identifies the length of the array,
+// appends part of the data to a list of buttons.
+// If the user selects one of those buttons, then the City, State, and Country details
+// are appended to the CITY card. The longitude and latitude are passed as parameters
+// through convertGeoToData function
+var displaySearchResult = function (city) {
+    if (city.length === 0) {
+        displayResultsEl.textContent = "City not found.";
+        return;
+    }
+
+    for (var i = 0; i < city.length; i++) {
+
+        var cityName = city[i].name;
+        var cityState = city[i].state;
+        var cityCountry = city[i].country;
+        var locationDetails = [];
+        var resultButtons = [];
+
+        locationDetails[i] = cityName + ", " + cityState + ", " + cityCountry;
+
+        resultButtons[i] = document.createElement("button");
+        resultButtons[i].textContent = locationDetails[i];
+        resultButtons[i].classList = "list-group-item list-group-item-primary";
+        resultButtons[i].setAttribute("data-select", i);
+
+        var cityListGroupUl = document.createElement("ul");
+        cityListGroupUl.classList = "list-group col-3";
+        cityListGroupUl.appendChild(resultButtons[i]);
+        displayResultsEl.appendChild(cityListGroupUl);
+
+        $(resultButtons[i]).on("click", function(event) {
+            var element = event.target;
+            var i = element.getAttribute('data-select');
+
+            displayResultsEl.remove();
+
+            var weatherLocation = document.createElement("div");
+            var weatherDetailsH3 = document.createElement("h3");
+            weatherDetailsH3.classList = "text-light";
+            weatherDetails.textContent = $(this).text();
+            weatherLocation.appendChild(weatherDetailsH3);
+
+            var latitude = city[i].lat;
+            var longitude = city[i].lon;
+
+            convertGeoToData(latitude, longitude);
+        });
+        
+    }
+};
+
+// convertGeoToData takes the latitude and longitude from the city data and adds
+// them to the apiUrl query. The result of that data is passed as a parameter through
+// the displayWeatherDetails function.
+var convertGeoToData = function(lat, lon) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon + "&units=imperial&exclude=hourly,dailer&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
     console.log(apiUrl);
     fetch(apiUrl)
         .then(function(response) {
@@ -22,51 +101,48 @@ var getWeatherData = function(input) {
                 console.log(response);
                 response.json().then(function(data) {
                     console.log(data);
-                    displaySearchResult(data)
+                    displayWeatherDetails(data);
                 });
             } else {
                 alert("Error: " + response.statusText)
             }
         })
         .catch(function(error) {
-            alert("Unable to connect to GitHub");
+            alert("Unable to connect to OpenWeather");
         });
-        // latitude = apiUrl[0].lat.value;
-        // console.log(latitude);
 };
 
-var displaySearchResult = function (city) {
-    if (city.length === 0) {
-        displayResults.textContent = "City not found.";
-        return;
-    }
+// displayWeatherDetails takes the One Call API data and pulls 
+// the  current temperature, wind, humidity, and UV index data.
+// Then appends that data to the CITY card below the City, State, and Sate details.
+var displayWeatherDetails = function(cityData) {
+    console.log(cityData.current);
+    var weatherCurrent = document.createElement("div");
 
-    for (var i = 0; i < city.length; i++) {
-        var locationDetails = "City: " + city[i].name + ", State: " + city[i].state + ", Country: " + city[i].country;
-        //console.log(locationDetails);
+    var weatherTemp = document.createElement("h3");
+    weatherTemp.classList = "text-light";
+    weatherTemp.textContent = "Temp: " + cityData.current.temp + " °F";
 
-        var cityListGroupUl = document.createElement("ul");
-        cityListGroupUl.classList = "list-group col-3";
+    var weatherWind = document.createElement("h3");
+    weatherWind.classList = "text-light";
+    weatherWind.textContent = "Wind: " + cityData.current.wind_speed + " MPH";
 
-        var cityListGroupLi = document.createElement("button");
-        cityListGroupLi.classList = "list-group-item list-group-item-primary test";
-        cityListGroupLi.setAttribute("data-choice", i);
-        cityListGroupLi.textContent = locationDetails;
+    var weatherHumidity = document.createElement("h3");
+    weatherHumidity.classList = "text-light";
+    weatherHumidity.textContent = "Humidity: " + cityData.current.humidity + " %";
 
-        cityListGroupUl.appendChild(cityListGroupLi);
+    var weatherUV = document.createElement("h3");
+    weatherUV.classList = "text-light";
+    weatherUV.textContent = "UV Index: " + cityData.current.uvi;
 
-        displayResults.appendChild(cityListGroupUl);
-
-        $('button[data-choice="' + i + '"]').on("click", function(index) {
-            console.log($(this).text());
-        });
-        
-    }
+    weatherCurrent.appendChild(weatherTemp);
+    weatherCurrent.appendChild(weatherWind);
+    weatherCurrent.appendChild(weatherHumidity);
+    weatherCurrent.appendChild(weatherUV);
+    weatherDetails.appendChild(weatherCurrent);
 };
 
-var buttonClickHandler = function() {
-    var input = inputValue.value.trim();
-    getWeatherData(input);
-};
 
-citySearchEl.addEventListener("click", buttonClickHandler);
+$(citySearchEl).on("click", function() {
+    buttonClickHandler();
+})
