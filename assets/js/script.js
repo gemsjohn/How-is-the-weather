418e50212aeb80e57090cca999778f6a21bfd5ad
JRM <johnrhmartin@gmail.com>
2022-02-07 17:35:04 -0500

    attempting to resolve a bug with http vs https

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,7 +126,7 @@ var buttonClickHandler = function(event) {
 // Add input parameter to Geocoding apiURL and look for a response.
 // Take response data and pass it through displaySearchResult() as a parameter.
 var getWeatherData = function(input) {
-    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=5&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + input + "&limit=5&appid=24787d0fd9b0c4521d7a7b1d914ee0c3";
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
